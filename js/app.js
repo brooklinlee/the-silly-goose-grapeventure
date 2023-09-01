ff39001d0fb5a568be141af2e9a728cf43257ff7
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-08-31 22:01:50 -0600

    correct button cached element references and test, create event listeners for each button and test

--- js/app.js
+++ js/app.js
@@ -1,17 +1,72 @@
 // Variables
 
 // Cached element references
-const toPageOne = document.querySelector('.page-1')
-const toPageTwo = document.querySelector('.page-2')
-const toPageThree = document.querySelector('.page-3')
-const toPageFour = document.querySelector('.page-4')
-const toPageFive = document.querySelector('.page-5')
-const toPageSix = document.querySelector('.page-6')
-const toPageSeven = document.querySelector('.page-7')
-const toPageEight = document.querySelector('.page-8')
-const toPageNine = document.querySelector('.page-9')
+const pgOneToSevenBtn = document.getElementById('pg-1-opt-1')
+const pgTwoToThreeBtn = document.getElementById('pg-2-opt-1')
+const pgTwoToFiveBtn = document.getElementById('pg-2-opt-2')
+const pgThreeToFourBtn = document.getElementById('pg-3-opt-1')
+const pgFourToOneBtn = document.getElementById('pg-4-opt-1')
+const pgFiveToSixBtn = document.getElementById('pg-5-opt-1')
+const pgFiveToSevenBtn = document.getElementById('pg-5-opt-2')
+const pgSixToFourBtn = document.getElementById('pg-6-opt-1')
+const pgSevenToSixBtn = document.getElementById('pg-7-opt-1')
+const pgSevenToEightBtn = document.getElementById('pg-7-opt-2')
+const pgEightToNineBtn = document.getElementById('pg-8-opt-1')
+const pgNineToFiveBtn = document.getElementById('pg-9-opt-1')
+
 // console.log(document.querySelector('.page-1'))
+console.log(pgOneToSevenBtn)
+console.log(pgTwoToThreeBtn)
+console.log(pgTwoToFiveBtn)
+console.log(pgThreeToFourBtn)
+console.log(pgFourToOneBtn)
+console.log(pgFiveToSixBtn)
+console.log(pgFiveToSevenBtn)
+console.log(pgSixToFourBtn)
+console.log(pgSevenToSixBtn)
+console.log(pgSevenToEightBtn)
+console.log(pgEightToNineBtn)
+console.log(pgNineToFiveBtn)
 
 // Event listeners
+pgOneToSevenBtn.addEventListener('click', function(){
+    console.log('event listener 1 working')
+})
+pgTwoToThreeBtn.addEventListener('click', function(){
+    console.log('event listener 2 working')
+})
+pgTwoToFiveBtn.addEventListener('click', function(){
+    console.log('event listener 3 working')
+})
+pgThreeToFourBtn.addEventListener('click', function(){
+    console.log('event listener 4 working')
+})
+pgFourToOneBtn.addEventListener('click', function(){
+    console.log('event listener 5 working')
+})
+pgFiveToSixBtn.addEventListener('click', function(){
+    console.log('event listener 6 working')
+})
+pgFiveToSevenBtn.addEventListener('click', function(){
+    console.log('event listener 7 working')
+})
+pgSixToFourBtn.addEventListener('click', function(){
+    console.log('event listener 8 working')
+})
+pgSevenToSixBtn.addEventListener('click', function(){
+    console.log('event listener 9 working')
+})
+pgSevenToEightBtn.addEventListener('click', function(){
+    console.log('event listener 10 working')
+})
+pgEightToNineBtn.addEventListener('click', function(){
+    console.log('event listener 11 working')
+})
+pgNineToFiveBtn.addEventListener('click', function(){
+    console.log('event listener 12 working')
+})
+
+
 
 // Functions
+function turnPage(){}
