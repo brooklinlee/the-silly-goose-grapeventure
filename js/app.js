a26721830a6165f352cba5169a4b184b791aff6d
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-01 14:53:02 -0600

    clean up and remove old code, write new pseudocode for game

--- js/app.js
+++ js/app.js
@@ -1,72 +1,22 @@
+// PSEUDO-CODE
+/* 
+Cached element references of each 'page' in HTML
+Create an array of the locations of each 'page' (div) in HTML
+
+Give each page a class of either 'active' or 'hidden'. Hidden classes will be given a CSS property of .hidden{display:none;}.
+
+Use event listeners on each button where 'click' has a function associated with it that will change the class of the targeted div from hidden to active or vice versa as necessary
+
+define a function for each page movement that will change all of the pages elements to hidden, then change the targeted page's class to active
+
+define an init() function that will at page initialization make sure that only page one's class is active and the rest are set to hidden
+
+*/
 // Variables
 
 // Cached element references
-const pgOneToSevenBtn = document.getElementById('pg-1-opt-1')
-const pgTwoToThreeBtn = document.getElementById('pg-2-opt-1')
-const pgTwoToFiveBtn = document.getElementById('pg-2-opt-2')
-const pgThreeToFourBtn = document.getElementById('pg-3-opt-1')
-const pgFourToOneBtn = document.getElementById('pg-4-opt-1')
-const pgFiveToSixBtn = document.getElementById('pg-5-opt-1')
-const pgFiveToSevenBtn = document.getElementById('pg-5-opt-2')
-const pgSixToFourBtn = document.getElementById('pg-6-opt-1')
-const pgSevenToSixBtn = document.getElementById('pg-7-opt-1')
-const pgSevenToEightBtn = document.getElementById('pg-7-opt-2')
-const pgEightToNineBtn = document.getElementById('pg-8-opt-1')
-const pgNineToFiveBtn = document.getElementById('pg-9-opt-1')
-
-// console.log(document.querySelector('.page-1'))
-console.log(pgOneToSevenBtn)
-console.log(pgTwoToThreeBtn)
-console.log(pgTwoToFiveBtn)
-console.log(pgThreeToFourBtn)
-console.log(pgFourToOneBtn)
-console.log(pgFiveToSixBtn)
-console.log(pgFiveToSevenBtn)
-console.log(pgSixToFourBtn)
-console.log(pgSevenToSixBtn)
-console.log(pgSevenToEightBtn)
-console.log(pgEightToNineBtn)
-console.log(pgNineToFiveBtn)
 
 // Event listeners
-pgOneToSevenBtn.addEventListener('click', function(){
-    console.log('event listener 1 working')
-})
-pgTwoToThreeBtn.addEventListener('click', function(){
-    console.log('event listener 2 working')
-})
-pgTwoToFiveBtn.addEventListener('click', function(){
-    console.log('event listener 3 working')
-})
-pgThreeToFourBtn.addEventListener('click', function(){
-    console.log('event listener 4 working')
-})
-pgFourToOneBtn.addEventListener('click', function(){
-    console.log('event listener 5 working')
-})
-pgFiveToSixBtn.addEventListener('click', function(){
-    console.log('event listener 6 working')
-})
-pgFiveToSevenBtn.addEventListener('click', function(){
-    console.log('event listener 7 working')
-})
-pgSixToFourBtn.addEventListener('click', function(){
-    console.log('event listener 8 working')
-})
-pgSevenToSixBtn.addEventListener('click', function(){
-    console.log('event listener 9 working')
-})
-pgSevenToEightBtn.addEventListener('click', function(){
-    console.log('event listener 10 working')
-})
-pgEightToNineBtn.addEventListener('click', function(){
-    console.log('event listener 11 working')
-})
-pgNineToFiveBtn.addEventListener('click', function(){
-    console.log('event listener 12 working')
-})
-
-
 
 // Functions
-function turnPage(){}
+
