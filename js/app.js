6c2bf1ec82fd2af85bf1a2d2727d6da371f2d300
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-08-31 20:54:02 -0600

    hide previous styling, create a section for each page of the game, add buttons, and create cached element references for each button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 // Variables
 
 // Cached element references
+const toPageOne = document.querySelector('.page-1')
+const toPageTwo = document.querySelector('.page-2')
+const toPageThree = document.querySelector('.page-3')
+const toPageFour = document.querySelector('.page-4')
+const toPageFive = document.querySelector('.page-5')
+const toPageSix = document.querySelector('.page-6')
+const toPageSeven = document.querySelector('.page-7')
+const toPageEight = document.querySelector('.page-8')
+const toPageNine = document.querySelector('.page-9')
+// console.log(document.querySelector('.page-1'))
 
 // Event listeners
 
