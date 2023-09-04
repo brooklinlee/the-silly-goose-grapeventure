280e3fc47ccb86300cdfa7c20b4b5e26da71c5bc
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-03 22:30:01 -0600

    manage css around button and story line placement

--- js/story.js
+++ js/story.js
@@ -2,7 +2,7 @@ const story = {
     pageTwo: ["Life is good as a silly goose... <br>&nbsp &nbsp &nbsp &nbsp and it's a beautiful day out!", "What do you want to do?",],
     pageThree: [`You got some <strong>bread crumbs</strong>! Hooray!`,`But for some reason, <strong> you can't stop thinking about grapes...</strong>`,],
     pageFive: [`You see a lemonade stand!`,],
-    pageSix:[`Lemonade isn't for you, br/ you silly goose!`,`Too bad he doesn't have any grapes...`,],
+    pageSix:[`Lemonade isn't for you, you silly goose!`,`Too bad he doesn't have any grapes...`,],
     pageSeven: [`The man said "No, we just sell lemonade,"`,`"But it's cold, and it's fresh, and it's all homemade!"`,`"Can I get you a glass?"`,],
     pageEight: [`So you waddle away...`,`... Till the very next day...`]
 }
